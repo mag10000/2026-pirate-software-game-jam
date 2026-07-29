03c86b516c856772bf2b4ead70708f56aa5fbaf6
Patrick Knisely <patrick_knisely@hotmail.com>
2026-07-28 19:19:33 -0500

    Fixed money

--- pc.gd
+++ pc.gd
@@ -232,20 +232,23 @@ func _on_break_timer_timeout():
 func raise_items_update():
 	match Global.day:
 		2:
-			Global.amt_earned_combos = 5
-			Global.amt_earned_icon = 2
+			Global.amt_earned_combos += 6
+			Global.amt_earned_icon +=2
 			Global.item_pool = ["res://inventory/items/money_multiplier_item.tres","res://inventory/items/time_add_item.tres","res://inventory/items/refresh_item.tres","res://inventory/items/bomb_item.tres", "res://inventory/items/lightning_item.tres"]
+			InventoryManager.grant_item("res://inventory/items/money_multiplier_item.tres", 1)
 		3:	
-			Global.amt_earned_combos = 6
-			Global.amt_earned_icon = 3
+			Global.amt_earned_combos += 6
+			Global.amt_earned_icon +=2
 			Global.item_pool = ["res://inventory/items/simplify_board_item.tres","res://inventory/items/money_multiplier_item.tres","res://inventory/items/time_add_item.tres","res://inventory/items/refresh_item.tres","res://inventory/items/bomb_item.tres", "res://inventory/items/lightning_item.tres"]
+			InventoryManager.grant_item("res://inventory/items/simplify_board_item.tres", 1)
 		4: 
-			Global.amt_earned_combos = 7
-			Global.amt_earned_icon = 4
+			Global.amt_earned_combos += 6
+			Global.amt_earned_icon +=2
 			Global.item_pool = ["res://inventory/items/missle_item.tres","res://inventory/items/simplify_board_item.tres","res://inventory/items/money_multiplier_item.tres","res://inventory/items/time_add_item.tres","res://inventory/items/refresh_item.tres","res://inventory/items/bomb_item.tres", "res://inventory/items/lightning_item.tres"]
+			InventoryManager.grant_item("res://inventory/items/missle_item.tres", 1)	
 		5: 
-			Global.amt_earned_combos = 8
-			Global.amt_earned_icon = 5
+			Global.amt_earned_combos += 6
+			Global.amt_earned_icon +=2
 			Global.item_pool = ["res://inventory/items/missle_item.tres","res://inventory/items/simplify_board_item.tres","res://inventory/items/money_multiplier_item.tres","res://inventory/items/time_add_item.tres","res://inventory/items/refresh_item.tres","res://inventory/items/bomb_item.tres", "res://inventory/items/lightning_item.tres"]
 		_:
 			pass

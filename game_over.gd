d7daf6cd99f831272021db65068877e354c07401
mag10000 <magnusknisely@gmail.com>
2026-07-30 09:26:56 -0500

    made changes

--- game_over.gd
+++ game_over.gd
@@ -4,3 +4,11 @@ extends Control
 func _on_button_pressed():
 	Global.clear_data()
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_leaderboard_pressed():
+	$LeaderBoard.show()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	$LeaderBoard.get_scores()
--- main_menu.gd
+++ main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 
 func _ready():
 	MusicPlayer.play()
+	cycle_leaderboard()
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://story_beat.tscn")
@@ -11,3 +12,15 @@ func _on_button_pressed():
 
 func _on_setttings_pressed():
 	$settings.show()
+
+
+func cycle_leaderboard():
+	await get_tree().create_timer(5).timeout
+	$LeaderBoard.show()
+	await get_tree().create_timer(5).timeout
+	$LeaderBoard.hide()
+	cycle_leaderboard()
+
+
+func _input(event):
+	$LeaderBoard.hide()

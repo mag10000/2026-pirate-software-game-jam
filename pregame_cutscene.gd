626beaed04d750748799bdb9beba1004cf2dc92f
Patrick Knisely <patrick_knisely@hotmail.com>
2026-07-30 17:48:03 -0500

    Cutscene

--- pregame_cutscene.gd
+++ pregame_cutscene.gd
@@ -15,25 +15,25 @@ func _process(delta):
 func play_cutscene():
 	await get_tree().create_timer(0.25).timeout
 	$AudioStreamPlayer.play()
-	type_text("Ziggy returns home after another long, wild night out on the town")
+	type_text("Ziggy returns home after another long, wild night out on the town.")
 	show_image(1)
 	await get_tree().create_timer(time_between_pics).timeout
 	show_image(2)
 	await get_tree().create_timer(time_between_pics).timeout
 	label.text = ""
-	type_text("Checking his bank account he sees his life catching up with him")
+	type_text("Checking his bank account he sees his life catching up with him.")
 	show_image(3)
 	await get_tree().create_timer(time_between_pics).timeout
 	show_image(4)
 	await get_tree().create_timer(time_between_pics).timeout
 	label.text = ""
-	type_text("“Oh man…. I’m gonna have to work so hard to pay this off…. WHAT THE HELL!?”")
+	type_text("“Oh man…. I’m gonna have to work so hard to pay this off... WHAT THE HELL!?”")
 	show_image(5)
 	await get_tree().create_timer(time_between_pics).timeout
 	show_image(6)
 	await get_tree().create_timer(time_between_pics).timeout
 	label.text = ""
-	type_text("“You have FIVE DAYS to pay off your debt… Or it’s LIGHTS OUT! Muahaha!!”")
+	type_text("“You have FIVE DAYS to pay off your debt... Or it’s LIGHTS OUT! Muahaha!!”")
 	show_image(7)
 	await get_tree().create_timer(time_between_pics).timeout
 	show_image(8)
@@ -46,7 +46,7 @@ func play_cutscene():
 func type_text(str: String):
 	for letter in str:
 		print("hi")
-		await get_tree().create_timer(0.05).timeout
+		await get_tree().create_timer(0.0375).timeout
 		label.text += letter
 #		if letter == ":" or letter == " ":
 #			await get_tree().create_timer(0.1).timeout

a29915868acd566e4c42ea56bf973b65fab34be5
Patrick Knisely <patrick_knisely@hotmail.com>
2026-07-30 17:08:18 -0500

    CUTSCENE

--- pregame_cutscene.gd
+++ pregame_cutscene.gd
@@ -15,52 +15,64 @@ func _process(delta):
 func play_cutscene():
 	await get_tree().create_timer(0.25).timeout
 	$AudioStreamPlayer.play()
-	type_text("Hello")
-	$Image1.show()
+	type_text("Ziggy returns home after another long, wild night out on the town")
+	show_image(1)
 	await get_tree().create_timer(2).timeout
-	$Image2.show()
+	show_image(2)
 	await get_tree().create_timer(2).timeout
-	$Image3.show()
+	label.text = ""
+	type_text("Checking his bank account he sees his life catching up with him")
+	show_image(3)
 	await get_tree().create_timer(2).timeout
-	$Image4.show()
+	show_image(4)
 	await get_tree().create_timer(2).timeout
-	type_text("")
+	label.text = ""
+	type_text("“Oh man…. I’m gonna have to work so hard to pay this off…. WHAT THE HELL!?”")
+	show_image(5)
 	await get_tree().create_timer(2).timeout
-	$Image5.show()
+	show_image(6)
 	await get_tree().create_timer(2).timeout
-	$Image6.show()
+	label.text = ""
+	type_text("“You have FIVE DAYS to pay off your debt… Or it’s LIGHTS OUT! Muahaha!!”")
+	show_image(7)
 	await get_tree().create_timer(2).timeout
-	type_text("")
-	await get_tree().create_timer(2).timeout
-	$Image7.show()
-	await get_tree().create_timer(2).timeout
-	$Image8.show()
-	type_text("")
+	show_image(8)
+	await get_tree().create_timer(2).timeout	
 	load_main_menu()
 	
-
 func type_text(str: String):
 	for letter in str:
 		print("hi")
-		await get_tree().create_timer(0.075).timeout
+		await get_tree().create_timer(0.005).timeout
 		label.text += letter
-		if letter == ":" or letter == " ":
-			await get_tree().create_timer(0.2).timeout
+#		if letter == ":" or letter == " ":
+#			await get_tree().create_timer(0.1).timeout
 
-func show_image(numb: int)
+func show_image(numb: int):
 	match numb:
 		1:
 			$Image1.show()
 		2:
 			$Image1.hide()
 			$Image2.show()
+		3:
+			$Image2.hide()
 			$Image3.show()
+		4:
+			$Image3.hide()
 			$Image4.show()
+		5:
+			$Image4.hide()
 			$Image5.show()
+		6:
+			$Image5.hide()
 			$Image6.show()
+		7:
+			$Image6.hide()
 			$Image7.show()
+		8:
+			$Image7.hide()
 			$Image8.show()
-		_:
-			pass
+
 func load_main_menu():
 	get_tree().change_scene_to_file("res://main_menu.tscn")

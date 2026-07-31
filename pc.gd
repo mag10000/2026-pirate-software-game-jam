d01716ae5297188f6f9e7cebdb3ac8284f244ca0
Patrick Knisely <patrick_knisely@hotmail.com>
2026-07-31 00:46:37 -0500

    test

--- pc.gd
+++ pc.gd
@@ -219,13 +219,13 @@ func _process(delta):
 			if Global.hour >= 8:
 				if deposited_in_round < Global.minimumPay:
 					get_tree().change_scene_to_file("res://game_over.tscn")
-				if Global.day == 5:
+				elif Global.day == 5:
 					get_tree().change_scene_to_file("res://you_win_credits.tscn")
 				else:
+					Global.day += 1
 					raise_items_update()
 					update_minimum_debt_payment()
 					# TODO - Create scene for new_day
-					Global.day += 1
 					get_tree().change_scene_to_file("res://day_win.tscn")
 			else:
 				await get_tree().create_timer(0.5).timeout
